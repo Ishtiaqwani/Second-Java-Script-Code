a63ed1cfc43f0d1de1bdb90910dc1341dfa99d7c
Ishtiaq Wani <Ishtiaq Wani>
2016-09-07 21:38:42 +0530

    my first commit

---
+++ ifelse.js
@@ -0,0 +1,19 @@
+console.log('is it working?');
+
+// I will make an array with different song lists
+
+var listofSongs= ['Pop', 'Classical', 'Rock'];
+
+//lets find third variable in the array list
+var thirdList= listofSongs[2];
+
+//I am making if...else...conditional to see if third variable is Rock in my list
+
+if (thirdList == 'Classical'){
+	console.log('the third list is of Classical music');
+}
+else if (listofSongs[2] == 'Pop') {
+	console.log ('the third list is of Pop music');
+}else{
+	console.log('The third list is Rock');
+}
